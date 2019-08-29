8152285c93d23280abfe8ef55c45273a6cce6858
Ben Kung <ben_kung@infofab.com>
2019-08-29 17:31:51 +0800

    ADD MODELS

--- webapi/src/models/model.ts
+++ webapi/src/models/model.ts
@@ -1,7 +1,28 @@
-export class Product { 
-    
+export class Product {
+    public productName: string;
+    public price: number;
 }
 
-export class Order { }
+export class Order {
+    public userName: string;
+    public productName: string;
+    public count: number;
+    public price: number;
+    public orderStatus: OrderStatus
 
-export class OrderView { }
+}
+
+export class OrderView {
+    public userName: string;
+    public productName: string;
+    public count: number;
+    public price: number;
+    public orderStatus: OrderStatus
+}
+
+export enum OrderStatus {
+    create,
+    pick,
+    ship,
+    payed
+}
