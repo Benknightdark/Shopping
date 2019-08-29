36d0c34253e3a3b41a678bf5419cdf7c4045a4ae
Ben Kung <ben_kung@infofab.com>
2019-08-29 17:20:24 +0800

    mark appcontgroller

--- README.md
+++ README.md
@@ -13,11 +13,21 @@
       - 出貨
       - 已付款
 - CommandBus
+  - orderProduct:'create' => 新增商品訂單，並將訂單狀態設為訂單成立
+  - orderProduct:'pick' => 修改商品訂單狀態為撿貨
+  - orderProduct:'ship' => 修改商品訂單狀態為出貨
+  - orderProduct:'payed' => 修改商品訂單狀態為已付款
 - EventBus
+  - orderView:'create' => 新增訂單檢視資料
+  - orderView:'update' => 修改訂單檢視資料
 # 應用程式相關資料庫
 - Business DB
+  - Product
+  - Order
 - Event Store
+  - OrderHistory
 - Query DB
+  - OrderView
 # 使用工具
 - NestJS
 - Angular
--- webapi/src/app.controller.ts
+++ webapi/src/app.controller.ts
@@ -1,37 +1,38 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
-import { AppService } from './app.service';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Cat, CreateCatDto } from './schemas/cat.schema';
-import { Client, Transport, ClientProxy } from '@nestjs/microservices';
-import { redisConnection } from './config/Environment';
-import { Observable } from 'rxjs';
+// import { Controller, Get, Post, Body, Param, Injectable } from '@nestjs/common';
+// import { AppService } from './app.service';
+// import { InjectModel } from '@nestjs/mongoose';
+// import { Model } from 'mongoose';
+// import { Cat, CreateCatDto } from './schemas/cat.schema';
+// import { Client, Transport, ClientProxy } from '@nestjs/microservices';
+// import { redisConnection } from './config/Environment';
+// import { Observable } from 'rxjs';
+// // @Injectable()
 
-@Controller('Cat')
-export class AppController {
-  constructor(
-    @InjectModel('Cat') private readonly catModel: Model<Cat>,
-  ) { }
-  @Post()
-  async create(@Body() createCatDto: CreateCatDto): Promise<Cat> {
-    const createdCat = new this.catModel(createCatDto);
-    return await createdCat.save();
-  }
-  @Get()
-  async findAll(): Promise<Cat[]> {
-    return await this.catModel.find().exec();
-  }
+// // @Controller('Cat')
+// // export class AppController {
+// //   constructor(
+// //     @InjectModel('CAT_MODEL') private readonly catModel: Model<Cat>,
+// //   ) { }
+// //   @Post()
+// //   async create(@Body() createCatDto: CreateCatDto): Promise<Cat> {
+// //     const createdCat = new this.catModel(createCatDto);
+// //     return await createdCat.save();
+// //   }
+// //   @Get()
+// //   async findAll(): Promise<Cat[]> {
+// //     return await this.catModel.find().exec();
+// //   }
 
-  @Client({
-    transport: Transport.REDIS,
-    options: {
-      url: redisConnection,
-    },
-  })
-  redisClient: ClientProxy;
-  @Get('command/:text')
-   command(@Param('text') text: string): Observable<string> {
-    const a: Observable<string> =  this.redisClient.send({ command: 'hi' }, text);
-    return a;
-  }
-}
+// //   @Client({
+// //     transport: Transport.REDIS,
+// //     options: {
+// //       url: redisConnection,
+// //     },
+// //   })
+// //   redisClient: ClientProxy;
+// //   @Get('command/:text')
+// //    command(@Param('text') text: string): Observable<string> {
+// //     const a: Observable<string> =  this.redisClient.send({ command: 'hi' }, text);
+// //     return a;
+// //   }
+// // }
