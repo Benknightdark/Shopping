74d6346e284d7a79679172b07de969d62215fa28
Ben Kung <ben_kung@infofab.com>
2019-08-30 11:01:40 +0800

    OK

--- webapi/src/product/product.service.ts
+++ webapi/src/product/product.service.ts
@@ -2,18 +2,21 @@ import { Injectable, Inject } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { ProductDto } from './dto/product-dto';
 import { IProduct } from './interfaces/product-interface';
+import { Observable, of, from } from 'rxjs';
 
 @Injectable()
 export class ProductService {
 
     constructor(@Inject('PRODUCT_MODEL') private readonly catModel: Model<IProduct>) { }
 
-    async create(createCatDto: ProductDto): Promise<IProduct> {
-        const createdCat = new this.catModel(createCatDto);
-        return await createdCat.save();
+    create(createCatDto: ProductDto): Observable<IProduct> {
+        const createdProduct = new this.catModel(createCatDto);
+        const $createdCat = from(createdProduct.save());
+        return $createdCat;
     }
 
-    async findAll(): Promise<IProduct[]> {
-        return await this.catModel.find().exec();
+    findAll(): Observable<IProduct[]> {
+        const data = from(this.catModel.find().exec());
+        return data;
     }
 }
